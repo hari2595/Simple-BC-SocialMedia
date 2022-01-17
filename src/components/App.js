cdb2f909c46a23b2fb935d3e6d40b9aec9be26fb
Hariharan Sasidharan <74772051+hari2595@users.noreply.github.com>
2022-01-17 17:38:33 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,11 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/hari2595"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Hari's network
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -21,23 +21,23 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/hari2595"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={logo} className="App-logo" alt="logo" width="400" height="500" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>Welcome</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/hari2595"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  hi <u><b>my github </b></u>
                 </a>
               </div>
             </main>
